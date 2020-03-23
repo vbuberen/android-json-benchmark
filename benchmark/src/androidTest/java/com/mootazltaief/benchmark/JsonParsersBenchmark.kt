bd74baffbfbaaa8dbfd4c918851a0e385268ac0d
Volodymyr Buberenko <v.buberenko@gmail.com>
2020-03-23 11:09:33 +0200

    Add LargeTest annotation in attempt to resolve timeout issues on some devices

--- benchmark/src/androidTest/java/com/mootazltaief/benchmark/JsonParsersBenchmark.kt
+++ benchmark/src/androidTest/java/com/mootazltaief/benchmark/JsonParsersBenchmark.kt
@@ -3,6 +3,7 @@ package com.mootazltaief.benchmark
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.mootazltaief.json.gson.GsonParser
 import com.mootazltaief.json.jackson.JacksonParser
@@ -12,6 +13,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class JsonParsersBenchmark {
 
